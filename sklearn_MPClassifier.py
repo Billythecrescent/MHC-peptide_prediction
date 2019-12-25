3d37fe49b4924c658d0209e952319635990777f0
Billythecrescent <13550378056@163.com>
2019-12-25 10:06:31 +0800

    update code

--- sklearn_MPClassifier.py
+++ sklearn_MPClassifier.py
@@ -13,15 +13,23 @@ import epitopepredict as ep
 codes = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
          'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
 
+
 def one_hot_encode(seq):
+    '''
+    Encode protein sequaence, seq, to a one-dimension array.
+    Encode the peptide using dummy code and join it with zeros.
+    input: [string] seq (length = n)
+    output: [1x20n ndarray] e
+    '''
     o = list(set(codes) - set(seq))
     s = pd.DataFrame(list(seq))    
     x = pd.DataFrame(np.zeros((len(seq),len(o)),dtype=int),columns=o)    
-    a = s[0].str.get_dummies(sep=',')
-    a = a.join(x)
-    a = a.sort_index(axis=1)
-    e = a.values.flatten()
-    print(a)
+    a = s[0].str.get_dummies(sep=',') #use dummy codes to encode the peptide
+    a = a.join(x) #join the peptide code with all-zero codes
+    a = a.sort_index(axis=1) #sort the DataFrame based on column
+    # print(a)
+    e = a.to_numpy().flatten() #convert to one-dimension array
+    # print(e)
     return e
 
 nlf = pd.read_csv('https://raw.githubusercontent.com/dmnfarrell/epitopepredict/master/epitopepredict/mhcdata/NLF.csv',index_col=0)
@@ -44,4 +52,5 @@ def random_encode(p):
     return [np.random.randint(20) for i in pep]
 
 pep='ALDFEQEMT'
-e=blosum_encode(pep)
+#e=blosum_encode(pep)
+e = one_hot_encode(pep)
