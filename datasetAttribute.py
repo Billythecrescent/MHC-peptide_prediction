77812a9351856f5976df9572f2be51ed99838b2a
Billythecrescent <13550378056@163.com>
2020-03-01 21:02:36 +0800

    add datasetDistribute function

--- datasetAttribute.py
+++ datasetAttribute.py
@@ -7,17 +7,39 @@ import pandas as pd
 import epitopepredict as ep
 
 
-def datasetAllele(dataset):
+def datasetAllele(dataset, rename = False, be_substituted = '', substitute = ''):
+    '''
+    'be_substituted' is the standard pattern which is to find
+        the char to be substituted. Default: '', referred: '[*]'.
+    'substitute' is the standard pattern which is to substitute
+        the char found by 'be_substituted' pattern.
+    '''
     alleles = dataset.allele.unique().tolist()
-    rename_allele = []
-    for allele in alleles:
-        rename_allele.append(re.sub('[*]','',allele))
-    return rename_allele
+    if rename == True:
+        renamed_allele = []
+        for allele in alleles:
+            renamed_allele.append(re.sub(be_substituted, substitute, allele))
+        return renamed_allele
+    else:
+        return alleles
+
+def datasetDistribute(dataset, format = None):
+    '''
+    Analyze the distrubution of the data frequency of alleles
+        in the dataset
+    'format' is the output format of the result, typically 'csv'
+        Default: None  
+    '''
+    alleles = dataset.allele.to_numpy()
+    # print(alleles)
+    allele_unique, allele_counts = np.unique(alleles, return_counts=True)
+    allele_counts_2d = pd.DataFrame(allele_counts, columns = ['counts'], index = allele_unique)
+    if format == 'csv':
+        allele_counts_2d.to_csv(os.path.join('dataset_alletes_distribution.csv'))
+    return allele_counts_2d
+
 
 evalset = ep.get_evaluation_set()
-# print(datasetAllele(evalset))
-a = ''
-for i in datasetAllele(evalset):
-    a = a + i +','
+# print(datasetAllele(evalset, True, '[*]', ''))
+print(datasetDistribute(evalset, 'csv'))
 
-print(a)
