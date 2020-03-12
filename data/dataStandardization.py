5f451700ceda006f76947bd763a2596439e5aa4b
Billythecrescent <13550378056@163.com>
2020-03-12 18:44:06 +0800

    normalize 9mer data, generate evalset_9mer_normalization.csv

--- data/dataStandardization.py
+++ data/dataStandardization.py
@@ -5,21 +5,29 @@ import numpy as np
 import pandas as pd
 import epitopepredict as ep
 
+module_path = os.path.dirname(os.path.abspath(__file__)) #path to module
+data_path = os.path.join(os.path.abspath(os.path.dirname(module_path)),"data")
+
 def data_8mer_normalization(filename):
     df = pd.read_csv(filename)
     alleles = df.allele.to_list()
     new_alleles = []
     for allele in alleles:
         str_groups = re.search(r'(.*\*)([0-9]{2})([0-9]{2})', allele).groups()
-        new_allele = str_groups[1] + str_groups[2] + ':' + str_groups[3]
-        print(new_allele)
+        new_allele = str_groups[0] + str_groups[1] + ':' + str_groups[2]
+        # print(new_allele)
         new_alleles.append(new_allele) 
     new_alleles_df = pd.DataFrame(new_alleles,columns=['allele'])
-    new_df = new_alleles_df+df['peptide']+df['ic50']+df['log50k']
+    # new_df = new_alleles_df+df['peptide']+df['ic50']+df['log50k']
+    new_df = pd.concat([new_alleles_df, pd.DataFrame(df, columns=['peptide', 'ic50', 'log50k'])], axis=1)
     return new_df
 
-file = "evalset_9mers.csv"
-print(data_8mer_normalization(file))
+# ##normalize_9mer
+# file = os.path.join(data_path, "evalset_9mers.csv")
+# # print(file)
+# # print(data_8mer_normalization(file))
+# df = data_8mer_normalization(file)
+# df.to_csv(os.path.join(data_path, 'evalset_9mer_normalization.csv'))
 
 def data_9mer_normalization(filename):
     return 
