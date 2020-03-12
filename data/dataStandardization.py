e95f93cf8b02511317f91194365c7c8ba13fbea3
Billythecrescent <13550378056@163.com>
2020-03-12 21:16:21 +0800

    add 11mer normalization, generate corresponding csv file

--- data/dataStandardization.py
+++ data/dataStandardization.py
@@ -62,7 +62,43 @@ def data_9mer_normalization(filename):
 # df.to_csv(os.path.join(data_path, 'evalset_9mer_normalization.csv'))
 
 def data_10mer_normalization(filename):
-    return 
+    df = pd.read_csv(filename)
+    alleles = df.allele.to_list()
+    new_alleles = []
+    for allele in alleles:
+        str_groups = re.search(r'([A-Z])([0-9]{2})([0-9]{2})', allele).groups()
+        new_allele = "HLA-" + str_groups[0] + "*" + str_groups[1] + ':' + str_groups[2]
+        # print(new_allele)
+        new_alleles.append(new_allele) 
+    new_alleles_df = pd.DataFrame(new_alleles,columns=['allele'])
+    # new_df = new_alleles_df+df['peptide']+df['ic50']+df['log50k']
+    new_df = pd.concat([new_alleles_df, pd.DataFrame(df, columns=['peptide', 'ic50', 'log50k'])], axis=1)
+    return new_df
+
+# ##normalize_10mer
+# file = os.path.join(data_path, "evalset_10mers.csv")
+# # # print(file)
+# # print(data_10mer_normalization(file))
+# df = data_10mer_normalization(file)
+# df.to_csv(os.path.join(data_path, 'evalset_10mer_normalization.csv'))
 
 def data_11mer_normalization(filename):
-    return 
+    df = pd.read_csv(filename)
+    alleles = df.allele.to_list()
+    new_alleles = []
+    for allele in alleles:
+        str_groups = re.search(r'([A-Z])([0-9]{2})([0-9]{2})', allele).groups()
+        new_allele = "HLA-" + str_groups[0] + "*" + str_groups[1] + ':' + str_groups[2]
+        # print(new_allele)
+        new_alleles.append(new_allele) 
+    new_alleles_df = pd.DataFrame(new_alleles,columns=['allele'])
+    # new_df = new_alleles_df+df['peptide']+df['ic50']+df['log50k']
+    new_df = pd.concat([new_alleles_df, pd.DataFrame(df, columns=['peptide', 'ic50', 'log50k'])], axis=1)
+    return new_df
+
+# ##normalize_11mer
+file = os.path.join(data_path, "evalset_11mers.csv")
+# # print(file)
+# print(data_11mer_normalization(file))
+df = data_11mer_normalization(file)
+df.to_csv(os.path.join(data_path, 'evalset_11mer_normalization.csv'))
